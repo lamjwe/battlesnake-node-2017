fd48fdd4dad1362cb5bb24190dbb06fb454eea7d
jonathanlam <lamjwe91@gmail.com>
2017-03-04 15:04:06 -0800

    test commit to test cause of problem

--- index.cc
+++ index.cc
@@ -43,7 +43,7 @@ void CPPAddon(const FunctionCallbackInfo<Value>& args) {
   v8::String::Utf8Value value(args[0]->ToString());
   std::string out_input = std::string(*value);
 
-  std::cout << "OUR OUTPUT : " << out_input;
+  // std::cout << "OUR OUTPUT : " << out_input;
   // Set the return value (using the passed in
   // FunctionCallbackInfo<Value>&)
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "TEST"));
