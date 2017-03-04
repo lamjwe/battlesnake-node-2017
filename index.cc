2b281d2ab3fbaca5e1fb5d1f25562c9b5a5ea992
jonathanlam <lamjwe91@gmail.com>
2017-03-04 15:05:30 -0800

    test commit to find problem

--- index.cc
+++ index.cc
@@ -41,7 +41,7 @@ void CPPAddon(const FunctionCallbackInfo<Value>& args) {
 
   // FROM http://stackoverflow.com/questions/16613828/how-to-convert-stdstring-to-v8s-localstring#16639079
   v8::String::Utf8Value value(args[0]->ToString());
-  std::string out_input = std::string(*value);
+  // std::string out_input = std::string(*value);
 
   // std::cout << "OUR OUTPUT : " << out_input;
   // Set the return value (using the passed in
